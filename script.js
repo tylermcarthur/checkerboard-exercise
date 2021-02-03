b99a58188d2d94183b093ab986bf71b7a10f5543
tylermcarthur <tylermcarthur@yahoo.com>
2021-02-03 17:23:08 -0600

    random colors answer

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 document.body.onload = makeGrid;
 function makeGrid(){
-    var colorIndicator = false;
+    function randomColor(){
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for(let i = 0; i < 6; i++){
+            color += letters[Math.floor(Math.random()* 16)];
+        }
+        return color;
+    }
     for(let i1 = 0; i1 < 9; i1++){
-        
         for(let i2 = 0; i2 < 9; i2++){
             const tile = document.createElement('div');
-            var color;
             tile.style.width = '11.1%';
             tile.style.float = 'left';
             tile.style.paddingBottom = '11.1%';
-            if(colorIndicator === false){
-                color = 'black';
-                colorIndicator = true;
-            } else {
-                color = 'red';
-                colorIndicator = false;
-            }
-            tile.style.backgroundColor = color;
+            tile.style.backgroundColor = (randomColor())
             document.body.appendChild(tile);
         }
         const newLine = document.createElement('div');
